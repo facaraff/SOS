e95908f359954523c7235a978554015eec3ef07b
facaraff <facaraff@VS>
2017-10-20 13:04:19 +0100

    sep-CMAES added
    Signed-off-by: facaraff <facaraff@VS>

--- BlasphMeme/src/algorithms/SEP_CMAES.java
+++ BlasphMeme/src/algorithms/SEP_CMAES.java
@@ -1,6 +1,5 @@
 package algorithms;
 
-
 import interfaces.Algorithm;
 import interfaces.Problem;
 import utils.RunAndStore.FTrend;
@@ -8,48 +7,44 @@ import utils.algorithms.cmaes.CMAEvolutionStrategy;
 
 import static utils.algorithms.Misc.toro;
 import static utils.algorithms.Misc.generateRandomSolution; 
-
-
-/**
+/*
  * Covariance Matrix Adaptation Evolutionary Strategy 
  */
-public class CMAES extends Algorithm
+public class SEP_CMAES extends Algorithm
 {
 	@Override
-	public FTrend execute(Problem problem, int maxEvaluations) throws Exception //this function implement the algorithm
+	public FTrend execute(Problem problem, int maxEvaluations) throws Exception 
 	{
 		FTrend FT = new FTrend(); 
 		int problemDimension = problem.getDimension();
 		double[][] bounds = problem.getBounds();
 
 		double[] best = new double[problemDimension];
-		double fBest = Double.NaN;
-		int j = 0;
 		if (initialSolution != null)
-		{
 			best = initialSolution;
-			fBest = initialFitness;
-		}
-		else //if not inserted, we need to randomly sample the initial guess
-		{
+		else
 			best = generateRandomSolution(bounds, problemDimension);
-			fBest = problem.f(best);
-			FT.add(j, fBest);
-			j++;
-		}
+		double fBest = Double.NaN;
+
 		// new a CMA-ES and set some initial values
 		CMAEvolutionStrategy cma = new CMAEvolutionStrategy();
+		//cma.parameters.setPopulationSize(10);
 		cma.setRepairMatrix(true);				// repair ill-conditioned matrix
 		cma.setDimension(problemDimension);		// overwrite some loaded properties
 		cma.setInitialX(best);					// in each dimension, also setTypicalX can be used
 		cma.setInitialStandardDeviation(0.2);	// also a mandatory setting 
 		cma.options.verbosity = -1;
 		cma.options.writeDisplayToFile = -1;
+		// XXX this parameter forces the use of a diagonal matrix, thus making the time complexity O(n) 
+		cma.options.diagonalCovarianceMatrix = 1;
 		
 		// initialize cma and get fitness array to fill in later
 		double[] fitness = cma.init();
-				
+		
+		//System.out.println(cma.getDataC());
+		
 		// iteration loop
+		int j = 0;
 		while (j < maxEvaluations)
 		{
             // --- core iteration step ---
@@ -58,19 +53,19 @@ public class CMAES extends Algorithm
 			
 			for(int i = 0; i < pop.length && j < maxEvaluations; ++i)
 			{ 
-				// saturate solution on bounds 
+				// saturate solution inside bounds 
 				pop[i] = toro(pop[i], bounds);
 				
 				// compute fitness/objective value	
 				fitness[i] = problem.f(pop[i]);
 				
 				// save best
-				if (fitness[i] < fBest)
+				if (j == 0 || fitness[i] < fBest)
 				{
 					fBest = fitness[i];
 					for (int n = 0; n < problemDimension; n++)
 						best[n] = pop[i][n];
-						FT.add(j, fBest);
+					FT.add(j, fBest);
 				}
 				
 				j++;
@@ -82,10 +77,8 @@ public class CMAES extends Algorithm
 		
 		finalBest = best;
 
-		//add the best solutionat the end f the fitness trend
 		FT.add(j, fBest);
 		
-		//return bests;
 		return FT;
 	}
 }
