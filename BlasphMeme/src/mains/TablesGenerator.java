6d45558aa8817d3736ac23f9a4cb63f23497ce4d
fcaraf00 <fcaraf00@GWHXTECHA10595S.lec-admin.dmu.ac.uk>
2017-12-08 11:55:46 +0000

    mains folder restored
    Signed-off-by: fcaraf00 <fcaraf00@GWHXTECHA10595S.lec-admin.dmu.ac.uk>

---
+++ BlasphMeme/src/mains/TablesGenerator.java
@@ -0,0 +1,59 @@
+package mains;
+import utils.resultsProcessing.Experiment;
+import utils.resultsProcessing.TableAvgStdStat;
+import utils.resultsProcessing.TableStatistics;
+import utils.resultsProcessing.TableHolmBonferroni;
+//import static utils.RunAndStore.slash;
+
+public class TablesGenerator
+{
+	public static void main (String args[]) throws Exception
+	{
+//		String s = slash();
+		String workingDir = "";
+		if (args.length < 1)
+		{
+			//workingDir ="../results/cec2015allDim";
+			workingDir ="C:\\Users\\fcaraf00\\Desktop\\FINAL\\UNROT";
+			//System.err.println("Usage: " + Analyse.class.getSimpleName() + " " + "RESULT_FOLDER");
+			//System.exit(-1);
+		}
+		else
+			workingDir = args[0];
+
+		Experiment experiment = new Experiment();
+		experiment.setDirectory(workingDir);
+		experiment.setTrendsFlag(true, false);
+		//experiment.setTrendsFlag(true);
+		experiment.importData();
+		experiment.describeExperiment();
+
+		// to save some memory...
+		/*
+		experiment.computeAVG();
+		experiment.computeSTD();
+		experiment.computeMedian();
+		experiment.deleteFinalValues();
+		*/
+
+		/*
+		Tests T1 = new TableCEC2013Competition(experiment);
+		T1.setErrorFlag(true);
+		T1.execute();
+
+		Tests T2 = new TableBestWorstMedAvgStd(experiment);
+		T2.setErrorFlag(true);
+		T2.execute();
+		 */
+		
+		TableHolmBonferroni T3 = new TableHolmBonferroni(experiment);
+		T3.setReferenceAlgorithm();
+		T3.execute();
+		
+
+		TableStatistics T4 = new TableAvgStdStat(experiment, true, true);
+		T4.setErrorFlag(true);
+		T4.setReferenceAlgorithm();
+		T4.execute();
+	}
+}
