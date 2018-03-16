424b940624cc94d03382b923adb1190a8664ddc8
fcaraf00 <fcaraf00@GWHXTECHA10595S.lec-admin.dmu.ac.uk>
2018-03-16 18:26:57 +0000

    eigeDE testing started!
    Signed-off-by: fcaraf00 <fcaraf00@GWHXTECHA10595S.lec-admin.dmu.ac.uk>

---
+++ BlasphMeme/src/algorithms/EigenDE2.java
@@ -0,0 +1,178 @@
+package algorithms;
+
+import  utils.algorithms.operators.DEOp;
+import static utils.algorithms.Misc.toro;
+import utils.algorithms.Misc;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.EigenDecomposition;
+
+import static utils.MatLab.multiply;
+import static utils.algorithms.Misc.Cov;
+import static utils.algorithms.Misc.generateRandomSolution;
+
+//import java.util.Vector; serve?
+
+import interfaces.Algorithm;
+import interfaces.Problem;
+import utils.RunAndStore.FTrend;
+
+/**
+ * Rotational Invariant Differential Evolution strategies by eigen-decomposition method 
+ */
+public class EigenDE2 extends Algorithm
+{
+	@Override
+	public FTrend execute(Problem problem, int maxEvaluations) throws Exception
+	{
+		int populationSize = getParameter("p0").intValue(); 
+		double F = getParameter("p1").doubleValue();
+		double CR = getParameter("p2").doubleValue();
+		int mutationStrategy = getParameter("p3").intValue();
+		int crossoverStrategy = getParameter("p4").intValue();
+		double alpha = getParameter("p5").doubleValue();
+		FTrend FT = new FTrend();
+		int problemDimension = problem.getDimension(); 
+		double[][] bounds = problem.getBounds();
+		
+		if(CR==-1)
+			CR = 1.0/Math.pow(2.0,1.0/(problemDimension*alpha));
+		
+		double[][] population = new double[populationSize][problemDimension];
+		double[] fitnesses = new double[populationSize];
+		
+		double[] best = new double[problemDimension];
+		double fBest = Double.NaN;
+		
+		int i = 0;
+		
+		// evaluate initial population
+		for (int j = 0; j < populationSize; j++)
+		{
+			double[] tmp = generateRandomSolution(bounds, problemDimension);
+			for (int n = 0; n < problemDimension; n++)
+				population[j][n] = tmp[n];
+			fitnesses[j] = problem.f(population[j]);
+			
+			if (j == 0 || fitnesses[j] < fBest)
+			{
+				fBest = fitnesses[j];
+				for (int n = 0; n < problemDimension; n++)
+					best[n] = population[j][n];
+					FT.add(i, fBest);
+			}
+			
+			i++;
+		}
+
+		// temp variables
+		double[][] Pt = null;//
+		double[][] rotPop = Misc.clone(population);
+		double[] currPt = new double[problemDimension];
+		double[] newPt = new double[problemDimension];
+		double[] crossPt = new double[problemDimension];
+		double currFit = Double.NaN;
+		double crossFit = Double.NaN;
+
+		// iterate
+		while (i < maxEvaluations)
+		{
+		
+			double[][] newPop = new double[populationSize][problemDimension];
+			Pt = changeCoorditate(rotPop);
+		
+			for (int j = 0; j < populationSize && i < maxEvaluations; j++)
+			{
+				for (int n = 0; n < problemDimension; n++)
+					currPt[n] = rotPop[j][n];
+				currFit = fitnesses[j];
+				
+				// mutation
+				switch (mutationStrategy)
+				{
+					case 1:
+						// DE/rand/1
+						newPt = DEOp.rand1(rotPop, F);
+						break;
+					case 2:
+						// DE/cur-to-best/1
+						newPt = DEOp.currentToBest1(rotPop, best, j, F);
+						break;
+					case 3:
+						// DE/rand/2
+						newPt = DEOp.rand2(rotPop, F);
+						break;
+					case 4:
+						// DE/rand-to-best/2
+						newPt = DEOp.randToBest2(rotPop, best, F);
+						break;
+					default:
+						break;
+				}
+						
+				// crossover
+				if (mutationStrategy != 4)
+				{
+					if (crossoverStrategy == 1)
+						crossPt = DEOp.crossOverBin(currPt, newPt, CR);
+					else if (crossoverStrategy == 2)
+						crossPt = DEOp.crossOverExp(currPt, newPt, CR);
+					else if (crossoverStrategy == 0)
+						crossPt = newPt;
+				}			
+				
+				crossPt = toro(restoreSystem(Pt,crossPt), bounds);
+				crossFit = problem.f(crossPt);
+				i++;
+
+
+				// replacement
+				if (crossFit < currFit)
+				{
+					for (int n = 0; n < problemDimension; n++)
+						newPop[j][n] = crossPt[n];
+					fitnesses[j] = crossFit;
+					
+					// best update
+					if (crossFit < fBest)
+					{
+						fBest = crossFit;
+						for (int n = 0; n < problemDimension; n++)
+							best[n] = crossPt[n];
+						//if(i==problemDimension)	
+						FT.add(i, fBest);
+					}
+				}
+				else
+				{
+					for (int n = 0; n < problemDimension; n++)
+						newPop[j][n] = population[j][n];
+					fitnesses[j] = currFit;
+				}
+				crossPt = null; newPt = null;
+			}
+			
+			population = newPop;
+			newPop = null;
+		}
+		
+		finalBest = best;
+		
+		FT.add(i, fBest);
+		return FT;		
+	}
+	
+	private double[][] changeCoorditate(double[][] pop)
+	{
+		EigenDecomposition E =  new EigenDecomposition(new Array2DRowRealMatrix(Cov(pop)));
+		double[][] P = E.getV().getData();
+		for(int i=0; i<pop.length; i++)
+			pop[i]= multiply(P,pop[i]);
+		return  E.getVT().getData();
+	}
+	
+	private double[] restoreSystem(double [][] Pt, double[] x)
+	{
+		return multiply(Pt,x);
+	}
+}
--- BlasphMeme/src/experiments/rotInvStudy/senzaXOr.java
+++ BlasphMeme/src/experiments/rotInvStudy/senzaXOr.java
@@ -4,6 +4,8 @@ import interfaces.Experiment;
 import interfaces.Algorithm;
 import benchmarks.CEC2014RotInvStudy;
 import algorithms.DE;
+import algorithms.EigenDE;
+import algorithms.EigenDE2;
 
 
 
@@ -24,6 +26,39 @@ public class senzaXOr extends Experiment
 //		a.setID("rotation");//N.B. this algorithm makes use of "generateRandomSolution" that has still to be implemented.
 //		add(a);
 		
+	
+		
+		a = new DE();
+		a.setID("DEr1exp");
+		a.setParameter("p0", (double)probDim);
+		a.setParameter("p1", 0.7);//F
+		a.setParameter("p2", -1.0); //CR
+		a.setParameter("p3", 1.0);
+		a.setParameter("p4", 2.0);
+		a.setParameter("p5", 0.3);//ALPHA
+		add(a);
+		
+		a = new EigenDE2();
+		a.setParameter("p0", (double)probDim);
+		a.setParameter("p1", 0.7);//F
+		a.setParameter("p2", -1.0); //CR
+		a.setParameter("p2", Double.NaN); //CR
+		a.setParameter("p3", 1.0);//mutation strategy
+		a.setParameter("p4", 2.0);//crossover strategy
+		a.setParameter("p5", 0.3);//ALPHA
+		add(a);
+		
+		a = new EigenDE();
+		a.setID("rEigenDEr1exp");
+		a.setParameter("p0", (double)probDim);
+		a.setParameter("p1", 0.7);//F
+		a.setParameter("p2", -1.0); //CR
+		a.setParameter("p3", 1.0);
+		a.setParameter("p4", 2.0);
+		a.setParameter("p5", 0.3);//ALPHA
+		a.setParameter("p6", 0.5);//PR
+		add(a);
+		
 		a = new DE();
 		a.setID("rDEr1enoxo");
 		a.setParameter("p0", (double)probDim);
@@ -34,7 +69,6 @@ public class senzaXOr extends Experiment
 		a.setParameter("p4", 0.0);//crossover strategy
 		a.setParameter("p5", 0.3);//ALPHA
 		add(a);
-		
 		
 		
 		for(int i = 1; i<=30; i++)
