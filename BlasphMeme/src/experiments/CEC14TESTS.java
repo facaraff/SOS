303a1c4361ad8f29e35f95c363e62d708b9f7a86
fcaraf00 <fcaraf00@GWHXTECHA10595S.lec-admin.dmu.ac.uk>
2018-03-17 15:56:16 +0000

    test vari
    Signed-off-by: fcaraf00 <fcaraf00@GWHXTECHA10595S.lec-admin.dmu.ac.uk>

---
+++ BlasphMeme/src/experiments/CEC14TESTS.java
@@ -0,0 +1,61 @@
+package experiments;
+
+import interfaces.Experiment;
+import interfaces.Algorithm;
+import benchmarks.CEC2014;
+//import algorithms.CMAES;
+//import algorithms.ISPO; 
+import algorithms.Powell;
+import algorithms.Powell_toro;
+import algorithms.Rosenbrock;
+
+
+public class CEC14TESTS extends Experiment
+{
+	
+	public CEC14TESTS(int probDim) throws Exception
+	{
+		//super(probDim,"cec2015allDim");
+		super(probDim,5000,"testCEC14");
+		setNrRuns(10);
+
+
+		Algorithm a;// ///< A generic optimiser.
+	    //Problem p;// ///< A generic problem.
+		
+		a = new Powell();
+		a.setParameter("p0", 0.0001);
+		a.setParameter("p1", 100.0);
+		add(a);
+		
+		a = new Powell_toro();
+		a.setParameter("p0", 0.0001);
+		a.setParameter("p1", 100.0);
+		add(a);
+		
+		a = new Rosenbrock();
+		a.setParameter("p0", 10e-5);
+		a.setParameter("p1", 2.0);
+		a.setParameter("p2", 0.5);
+		add(a);
+		
+//		a = new CMAES(); //N.B. this algorithm makes use of "generateRandomSolution" that has still to be implemented.
+//		add(a);
+
+//		a = new ISPO();
+//		a.setParameter("p0", 1.0);
+//		a.setParameter("p1", 10.0);
+//		a.setParameter("p2", 2.0);
+//		a.setParameter("p3", 4.0);
+//		a.setParameter("p4", 1e-5);
+//		a.setParameter("p5", 30.0);
+//		add(a);
+	
+		for(int i = 1; i<=30; i++)
+			add(new CEC2014(probDim, i));
+
+
+
+
+	}
+}
