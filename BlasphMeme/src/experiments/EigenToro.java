98aaf7ac718ee0afbf426ead031b37c5b1ee17b5
fcaraf00 <fcaraf00@GWHXTECHA10595S.lec-admin.dmu.ac.uk>
2018-04-10 17:45:14 +0100

    collaborare col toro?
    Signed-off-by: fcaraf00 <fcaraf00@GWHXTECHA10595S.lec-admin.dmu.ac.uk>

---
+++ BlasphMeme/src/experiments/EigenToro.java
@@ -0,0 +1,116 @@
+package experiments;
+
+import interfaces.Experiment;
+import interfaces.Algorithm;
+import benchmarks.CEC2014;
+//import algorithms.RIDE;
+//import algorithms.DE;
+//import algorithms.MMCDE;
+import algorithms.EigenDE;
+
+
+
+public class EigenToro extends Experiment
+{
+	
+	public EigenToro(int probDim) throws Exception
+	{
+		//super(probDim,"cec2015allDim");
+		super(probDim,5000,"testCEC14Eigen");
+		setNrRuns(30);
+
+
+		Algorithm a;// ///< A generic optimiser.
+	    //Problem p;// ///< A generic problem.
+		
+
+//		a = new DE();
+//		a.setID("rDEr1bin");
+//		a.setParameter("p0", (double)probDim);
+//		a.setParameter("p1", 0.7);//F
+//		a.setParameter("p2", -1.0); //CR
+//		a.setParameter("p3", 1.0);
+//		a.setParameter("p4", 1.0);
+//		a.setParameter("p5", 0.3);//ALPHA
+//		add(a);
+//		
+//		a = new DE();
+//		a.setID("rDEr1exp");
+//		a.setParameter("p0", (double)probDim);
+//		a.setParameter("p1", 0.7);//F
+//		a.setParameter("p2", -1.0); //CR
+//		a.setParameter("p3", 1.0);
+//		a.setParameter("p4", 2.0);
+//		a.setParameter("p5", 0.3);//ALPHA
+//		add(a);
+//		
+//		a = new DE();
+//		a.setID("rDEctr1");
+//		a.setParameter("p0", (double)probDim);
+//		a.setParameter("p1", 0.7); //F
+//		a.setParameter("p2", Double.NaN);
+//		a.setParameter("p3", 4.0);
+//		a.setParameter("p4", Double.NaN);
+//		a.setParameter("p5", Double.NaN);//ALPHA
+//		add(a);
+//		
+//		
+//		a = new MMCDE();
+//		a.setID("rMMCDE");
+//		a.setParameter("p0", (double)probDim);
+//		a.setParameter("p1", 0.7); //F
+//		a.setParameter("p2", -1.0); //CR
+//		a.setParameter("p3", 0.3);//ALPHA
+//		add(a);
+//		
+//		a = new RIDE();
+//		a.setID("rRIDErand1bin");
+//		a.setParameter("p0", (double)probDim);
+//		a.setParameter("p1", 0.7); //F
+//		a.setParameter("p2", -1.0); //CR
+//		a.setParameter("p3", 1.0);
+//		a.setParameter("p4", 4.0);
+//		a.setParameter("p5", 0.3);//ALPHA
+//		add(a);
+//		
+//		a = new RIDE();
+//		a.setID("rRIDErand1exp");
+//		a.setParameter("p0", (double)probDim);
+//		a.setParameter("p1", 0.7); //F
+//		a.setParameter("p2", -1.0); //CR
+//		a.setParameter("p3", 1.0);
+//		a.setParameter("p4", 5.0);
+//		a.setParameter("p5", 0.3);//ALPHA
+//		add(a);
+		
+		a = new EigenDE();
+		a.setID("rEigenDEr1bin");
+		a.setParameter("p0", (double)probDim);
+		a.setParameter("p1", 0.7);//F
+		a.setParameter("p2", 0.3); //CR
+		a.setParameter("p3", 1.0);
+		a.setParameter("p4", 1.0);
+		a.setParameter("p5", Double.NaN);//ALPHA
+		a.setParameter("p6", 1.0);//PR
+		add(a);
+		
+		a = new EigenDE();
+		a.setID("rEigenDEr1exp");
+		a.setParameter("p0", (double)probDim);
+		a.setParameter("p1", 0.7);//F
+		a.setParameter("p2", -1.0); //CR
+		a.setParameter("p3", 1.0);
+		a.setParameter("p4", 2.0);
+		a.setParameter("p5", 0.3);//ALPHA
+		a.setParameter("p6", 1.0);//PR
+		add(a);
+		
+		
+		add(new CEC2014(probDim, 8));
+		add(new CEC2014(probDim, 10));
+
+
+
+
+	}
+}
