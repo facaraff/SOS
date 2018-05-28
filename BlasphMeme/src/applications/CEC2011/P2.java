e7e1ec07de975017a967615b1f59e13c2f9e4f50
orzobimbo <orzobimbo@mainagioia>
2018-05-28 21:54:17 +0200

    lennard has to be re-implemented!!!!!!

--- BlasphMeme/src/applications/CEC2011/P2.java
+++ BlasphMeme/src/applications/CEC2011/P2.java
@@ -22,39 +22,43 @@ public class P2 extends Problem { //LENNARD-JONES POTENTIAL PROBLEM
 	
 	public double f(double[] x){return evaluatePotential(x);}
 		
-		private double[][] generateBounds(int probDim)
+	private double[][] generateBounds(int probDim)
+	{
+		double[][] bounds = new double[probDim][2];
+		bounds[0][0] = 0;  bounds[1][0] = 0; bounds[2][0] = 0;
+		bounds[0][1] = 4;  bounds[1][1] = 4; bounds[2][1] = Math.PI;
+			
+		for (int i = 3; i < probDim; i++)
 		{
-			double[][] bounds = new double[probDim][2];
-			bounds[0][0] = 0;  bounds[1][0] = 0; bounds[2][0] = 0;
-			bounds[0][1] = 4;  bounds[1][1] = 4; bounds[2][1] = Math.PI;
-			
-			for (int i = 3; i < probDim; i++)
-			{
-				bounds[i][0] = -4 - 0.25*Math.floor((i-3)/3);
-				bounds[i][1] =  4 + 0.25*Math.floor((i-3)/3);
-			}
-			
-			return bounds;
+			bounds[i][0] = -4 - 0.25*Math.floor((i-3)/3);
+			bounds[i][1] =  4 + 0.25*Math.floor((i-3)/3);
 		}
+			
+		return bounds;
+	}
 		
-		/**
-		 * Fitness function (Lennard-Jones potential)
-		 * @param x
-		 * @return fitness
-		 */
-		private static double evaluatePotential(double[] x)
-		{
-			double v = 0;
-			double r = Double.NaN;
-			int n = x.length/3;
-			double[][] X = reShape(x,3,n);
-			X = transpose(X);
-			for(int i=0; i<n-1; i++)
-				for(int j=i+1; j<n; j++)
-				{ 
-					r = norm2(subtract(X[i],X[j]));
-					v += Math.pow(r,-12) -2*Math.pow(r, -6);
-				}
-			return v;
-		}
+	/**
+	 * Fitness function (Lennard-Jones potential)
+	 * @param x
+	 * @return fitness
+	 */
+	private static double evaluatePotential(double[] x)
+	{
+		double v = 0;
+		double r = Double.NaN;
+		int n = x.length/3;
+		double[][] X = reShape(x,3,n);
+		X = transpose(X);
+		for(int i=0; i<n-1; i++)
+			for(int j=i+1; j<n; j++)
+			{
+				r = norm2(subtract(X[i],X[j])); //XXX can be equal to 0?
+				if(r==0) 
+					v += Double.MAX_VALUE; //System.out.println("r "+r);
+				//if(r!=0) 
+				else
+				   v += (Math.pow(r,-12) -2*Math.pow(r, -6)); //System.out.println("v "+v);
+			}
+		return v;
+	}
 }
--- BlasphMeme/src/experiments/TestCEC2011.java
+++ BlasphMeme/src/experiments/TestCEC2011.java
@@ -4,29 +4,47 @@ import interfaces.Experiment;
 import interfaces.Algorithm;
 import interfaces.Problem;
 import algorithms.CMAES;
-import algorithms.singleSolution.ISPO;
-import applications.CEC2011.P7;
+import algorithms.MDE_pBX;
+import algorithms.CCPSO2;
+import algorithms.singleSolution.SPAMAOS2;
+//import algorithms.singleSolution.ISPO;
+//import applications.CEC2011.P7;
+//import applications.CEC2011.P1;
+import applications.CEC2011.P2;
 
 
 public class TestCEC2011 extends Experiment
 {
-	
 	public TestCEC2011()
 	{
 		super("TESTCEC2011");
-		setNrRuns(10);	
+		setNrRuns(30);	
 		
 		Algorithm a;// ///< A generic optimiser.
 	    Problem p;// ///< A generic problem.
-
-		a = new ISPO();
-		a.setParameter("p0", 1.0);
-		a.setParameter("p1", 10.0);
-		a.setParameter("p2", 2.0);
-		a.setParameter("p3", 4.0);
-		a.setParameter("p4", 1e-5);
-		a.setParameter("p5", 30.0);
-		add(a); //add it to the list
+	    
+	    a = new SPAMAOS2();
+		a.setParameter("p0",0.5);
+		a.setParameter("p1", 0.4); 
+		a.setParameter("p2",150.0);
+		a.setParameter("p3",2.0);
+		a.setParameter("p4",0.5);
+		a.setParameter("p5",0.00001);
+		a.setParameter("p6",3.0); //{1,2,3,4}
+		add(a);
+	    
+	    a = new MDE_pBX();
+		a.setParameter("p0", 100.0);
+		a.setParameter("p1", 0.15); 
+		a.setParameter("p2", 0.6);
+		a.setParameter("p3", 0.5);
+		a.setParameter("p4", 1.5);
+	    add(a);
+	    
+	    a = new CCPSO2();
+	    a.setParameter("p0", 30.0);
+	    a.setParameter("p1", 0.5);
+	    add(a);
 
 		a = new CMAES(); //N.B. this algorithm makes use of "generateRandomSolution" that has still to be implemented.
 		add(a);
@@ -37,10 +55,26 @@ public class TestCEC2011 extends Experiment
 		//p = new Ackley(problemDimension, bounds);
 //		p = new P1();
 //		add(p);//add it to the list
+//		
+//		p = new P7();
+//		add(p);//add it to the list
 		
-		p = new P7();
+		p = new P2(30);
 		add(p);//add it to the list
+//		p = new P2(60);
+//		add(p);//add it to the list
+//		p = new P2(90);
+//		add(p);//add it to the list
+//		p = new P2(120);
+//		add(p);//add it to the list
+//		p = new P2(300);
+//		add(p);//add it to the list
+//		p = new P2(600);
+//		add(p);//add it to the list
+//		p = new P2(900);
+//		add(p);//add it to the list
 
+		
 
 	}
 }
