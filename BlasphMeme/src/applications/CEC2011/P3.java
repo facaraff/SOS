7793d8f8f790b0dcb8f1a8aa696783df27629000
dicdo <dicdo@192.168.1.75>
2018-11-05 22:40:26 +0000

    To check again P3: -return negative value compare to matlab code (eq correct compared to 	the paper) P4: -the Riemman Integration is applied (Matlab no) 	-value to be initialized between 0 and 5, but no constrain. How to do?

--- BlasphMeme/src/applications/CEC2011/P3.java
+++ BlasphMeme/src/applications/CEC2011/P3.java
@@ -2,81 +2,81 @@ package applications.CEC2011;
 
 import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
 import org.apache.commons.math3.ode.FirstOrderIntegrator;
+import org.apache.commons.math3.ode.nonstiff.ClassicalRungeKuttaIntegrator;
 import org.apache.commons.math3.ode.nonstiff.DormandPrince853Integrator;
 
 
 import interfaces.Problem;
 
 
-public class P3 extends Problem implements FirstOrderDifferentialEquations{ // Bifunctional Catalyst Blend Optimal Control Problem
-
-	//Matrix Coefficent (Table 1 - Report CEC2011)
-	static double[][] c = { 
-			{	0.002918487,	-0.008045787,	0.006749947,	-0.001416647 },
-			{	9.509977,		-35.00994,		42.83329,		-17.33333	 },
-			{	26.82093,		-95.56079,		113.0398,		-44.29997	 },
-			{	208.7241,		-719.8052,		827.7466,		-316.6655	 },
-			{	1.350005,		-6.850027,		12.16671,		-6.666689	 },
-			{	0.01921995,		-0.0794532,		0.110566,		-0.05033333	 },
-			{	0.1323596,		-0.469255,		0.5539323,		-0.2166664	 },
-			{	7.339981,		-25.27328,		29.93329,		-11.99999	 },
-			{	-0.3950534,		1.679353,		-1.777829,		0.4974987	 },
-			{	-2.504665E-5,	0.01005854,		-0.01986696,	0.00983347	 }
-		};
+public class P3 extends Problem { // Bifunctional Catalyst Blend Optimal Control Problem
 	
-	double u;
-
-	
-	
-	public P3(int probDim) //throws Exception
+	public P3() //throws Exception
 	{		
-		super(probDim, new double[] {0.6, 0.9}); 		
+		super(1, new double[] {0.6, 0.9}); 		
 	}
 	
-	public double f(double[] x){
+	public double f(double[] x) throws Exception{
 		
-		u=x[0];
-		
+		//Initial State
 		double[] y=new double[] {1,0,0,0,0,0,0};
 		
+		//Final State
+		double[] yDot=new double[7];
+		
+		DiffEquations DE = new DiffEquations(x[0]);
+		
 		FirstOrderIntegrator intgr=new DormandPrince853Integrator(1.0e-8, 100.0, 1.0e-10, 1.0e-10);
-		
-		intgr.integrate(this, 0, y, 0.78, y);
-		
-		return y[6]*1000;
-	}
-	
-	public int getDimension() {//XXX (Fabio) this overrided method can be removed as the dimensionaliy if hte peoblem is fixed and therefore can be specified at constructor level
-		return 7;
-	}
-			
-	private double[] equations(double[] y, double ut) 
-	{
-		double[] dy = new double[7];
-		double[] k = new double[10];
-		
-		
-		//Equations 14 Report CEC2011
-		for(int i=0; i<10; i++) 
-			k[i] = c[i][0] + c[i][1]*ut + c[i][2]*Math.pow(ut, 2) + c[i][3]*Math.pow(ut,3);
+		//FirstOrderIntegrator intgr=new ClassicalRungeKuttaIntegrator(1.0e-3);
 				
-		//Equations 4,8-13 Report CEC2011
-		dy[0]= -k[0]*y[0];
-		dy[1]= k[0]*y[0] - (k[1] + k[2])*y[1] + k[3]*y[4];
-		dy[2]= k[1]*y[1];
-		dy[3]= -k[5]*y[3] + k[4]*y[4];
-		dy[4]= -k[2]*y[1] + k[5]*y[3] - (k[3] + k[4] + k[7] + k[8])*y[4] + k[6]*y[5] + k[9]*y[6];
-		dy[5]= k[7]*y[4] - k[6]*y[5];
-		dy[6]= k[8]*y[4] - k[9]*y[6];
+		intgr.integrate(DE, 0, y, 0.78, yDot);
 		
-		return dy;
+		return yDot[6]*1000;
 	}
 	
-	public void computeDerivatives(double t, double[] y, double[] yDot) {
+	private class DiffEquations implements FirstOrderDifferentialEquations {
 		
-		//Need to be check, matlab code just work with one value
-		// u need to be consider value in t
-		yDot=equations(y,u);
+		//Matrix Coefficient (Table 1 - Report CEC2011)
+		double[][] c=new double[][] {	{	2.918487E-3,	-8.045787E-3,	6.749947E-3,	-1.416647E-3 },
+										{	9.509977E0,		-3.500994E1,	4.283329E1,		-1.733333E1	 },
+										{	2.682093E1,		-9.556079E1,	1.130398E2,		-4.429997E1	 },
+										{	2.087241E2,		-7.198052E2,	8.277466E2,		-3.166655E2	 },
+										{	1.350005E0,		-6.850027E0,	1.216671E1,		-6.666689E0	 },
+										{	1.921995E-2,	-7.945320E-2,	1.105660E-1,	-5.033333E-2 },
+										{	1.323596E-1,	-4.692550E-1,	5.539323E-1,	-2.166664E-1 },
+										{	7.339981E0,		-2.527328E1,	2.993329E1,		-1.199999E1	 },
+										{	-3.950534E-1,	1.679353E0,		-1.777829E0,	4.974987E-1	 },
+										{	-2.504665E-5,	1.005854E-2,	-1.986696E-2,	9.833470E-3	 }
+									}; 
+		
+									
+		private double[] k = new double[10];
+		private double u;
+		
+		public DiffEquations(double x) {
+			u=x;
+			
+			//Equations 14 Report CEC2011
+			for(int i=0; i<10; i++) 
+				k[i] = c[i][0] + c[i][1]*u + c[i][2]*Math.pow(u, 2) + c[i][3]*Math.pow(u,3);
+		}
+		
+		public int getDimension() {
+			return 7;
+		}
+		
+		public void computeDerivatives(double t, double[] y, double[] yDot) {
+			 				
+			//Equations 4,8-13 Report CEC2011
+			yDot[0]= -k[0]*y[0];
+			yDot[1]= k[0]*y[0] - (k[1] + k[2])*y[1] + k[3]*y[4];
+			yDot[2]= k[1]*y[1];
+			yDot[3]= -k[5]*y[3] + k[4]*y[4];
+			yDot[4]= -k[2]*y[1] + k[5]*y[3] - (k[3] + k[4] + k[7] + k[8])*y[4] + k[6]*y[5] + k[9]*y[6];
+			yDot[5]= k[7]*y[4] - k[6]*y[5];
+			yDot[6]= k[8]*y[4] - k[9]*y[6];
+			
+		}
 		
 	}
 
---
+++ BlasphMeme/src/applications/CEC2011/P4.java
@@ -0,0 +1,77 @@
+package applications.CEC2011;
+
+import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
+import org.apache.commons.math3.ode.FirstOrderIntegrator;
+import org.apache.commons.math3.ode.nonstiff.ClassicalRungeKuttaIntegrator;
+import org.apache.commons.math3.ode.nonstiff.DormandPrince853Integrator;
+import org.apache.commons.math3.ode.sampling.StepHandler;
+import org.apache.commons.math3.ode.sampling.StepInterpolator;
+
+import interfaces.Problem;
+
+
+public class P4 extends Problem { // Optimal Control of a Non-Linear Stirred Tank Reactor
+	
+	double yInt,u;
+	
+	public P4() //throws Exception
+	{		
+		super(1,new double[] {0,0.5}); 		
+	}
+	
+
+	
+	public double f(double[] x) throws Exception{
+		
+		//Initial State
+		double[] y=new double[] {0.09,0.09};
+		
+		u=x[0];
+		yInt=0;
+		
+		DiffEquations DE = new DiffEquations();
+		
+		FirstOrderIntegrator intgr=new DormandPrince853Integrator(1.0e-8, 1, 1.0e-10, 1.0e-10);
+		//FirstOrderIntegrator intgr=new ClassicalRungeKuttaIntegrator(1.0e-2);
+	
+		
+		intgr.addStepHandler(stepHandler);				
+		intgr.integrate(DE, 0, y, 0.72, y); // To replace final time with 0.78
+	
+		
+		return yInt;
+	}
+	
+	private class DiffEquations implements FirstOrderDifferentialEquations {
+		
+		public int getDimension() {
+			return 2;
+		}
+		
+		public void computeDerivatives(double t, double[] y, double[] yDot) {
+			 				
+			//Equations 16-17 Report CEC2011
+			yDot[0]= -(2+u)*(y[0]+0.25)+(y[1]+0.5)*Math.exp(25*y[0]/(y[0]+2));
+			yDot[1]= 0.5-y[1]-(y[1]+0.5)*Math.exp(25*y[0]/(y[0]+2));
+			
+		}
+		
+	}
+	
+	StepHandler stepHandler = new StepHandler() {
+	    public void init(double t0, double[] y0, double t) {
+	    }
+	            
+	    public void handleStep(StepInterpolator interpolator, boolean isLast) {
+	    	
+	    	double   t = interpolator.getCurrentTime();
+	    	double   told= interpolator.getPreviousTime();
+	    	double[] y = interpolator.getInterpolatedState();
+	        
+	        //Evaluate Continuously The Integration usin Riemann (Code Matlab non lo fanno)
+	        yInt=yInt+(Math.pow(y[0],2)+Math.pow(y[1],2)+0.1*Math.pow(u,2))*(t-told);
+	        
+	    }
+	};
+
+}
