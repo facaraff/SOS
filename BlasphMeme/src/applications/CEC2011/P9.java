ff60fb99757172e7b2148a23554674921dfc3615
p15082278 <p15082278@HUAXXBAL023539S.lec-admin.dmu.ac.uk>
2019-03-11 20:20:45 +0000

    Runna, da finire di controllare i risultati con il codice matlaba

--- BlasphMeme/src/applications/CEC2011/P9.java
+++ BlasphMeme/src/applications/CEC2011/P9.java
@@ -18,7 +18,6 @@ public class P9 extends Problem {
 	Double[] pg,pd;
 	Integer[] gbus,dbus;
 	double[][] bt;
-	double[] pg2,pd2;
 	
 	double[][] YIbus,Xbus;
 	double[][][] ptdf;
@@ -44,86 +43,85 @@ public class P9 extends Problem {
 		
 		bus = new busSpecification[30];
 		
-		bus[0]= new busSpecification(1,1,1.05,0.0,0.0,0.0,165.9,0,0,0,0.000);
-		bus[1]= new busSpecification(2,2,1.043,0.0,21.7,12.7,49.1,28.4,-40,50,0.000);
-		bus[2]= new busSpecification(3,0,1.00,0.0,2.4,1.2,0.0,0.0,0,0,0.000);
-		bus[3]= new busSpecification(4,0,1.00,0.0,7.6,1.6,0.0,0.0,0,0,0.0);
-		bus[4]= new busSpecification(5,2,1.01,0.0,94.2,19.0,21.6,28,-40,40,0.0);
-		bus[5]= new busSpecification(6,0,1.00,0.0,0.0,0.0,0.0,0.0,0,0,0.0);
-		bus[6]= new busSpecification(7,0,1.00,0.0,22.8,10.9,0.0,0.0,0,0,0.0);
-		bus[7]= new busSpecification(8,2,1.01,0.0,30.0,30.0,22.8,39.1,-6,24,0.0);
-		bus[8]= new busSpecification(9,0,1.00,0.0,0.0,0.0,0.0,0.0,0,0,0.0);
-		bus[9]= new busSpecification(10,0,1.00,0.0,5.8,2.0,0.0,0.0,-6,24,0.0);
-		bus[10]= new busSpecification(11,2,1.082,0.0,0.0,0.0,12.4,31.6,0,0,0.0);
-		bus[11]= new busSpecification(12,0,1.00,0.0,11.2,7.5,0.0,0.0,0,0,0.0);
-		bus[12]= new busSpecification(13,2,1.071,0.0,0.0,0.0,11.6,45.7,0,0,0.0);
-		bus[13]= new busSpecification(14,0,1.0,0.0,6.2,1.6,0.0,0.0,0,0,0.0);
-		bus[14]= new busSpecification(15,0,1.0,0.0,8.2,2.5,0.0,0.0,0,0,0.0);
-		bus[15]= new busSpecification(16,0,1.0,0.0,3.5,1.8,0.0,0.0,0,0,0.0);
-		bus[16]= new busSpecification(17,0,1.0,0.0,9.0,5.8,0.0,0.0,0,0,0.0);
-		bus[17]= new busSpecification(18,0,1.0,0.0,3.2,0.9,0.0,0.0,0,0,0.0);
-		bus[18]= new busSpecification(19,0,1.0,0.0,9.5,3.4,0.0,0.0,0,0,0.0);
-		bus[19]= new busSpecification(20,0,1.0,0.0,2.2,0.7,0.0,0.0,0,0,0.0);
-		bus[20]= new busSpecification(21,0,1.0,0.0,17.5,11.2,0.0,0.0,0,0,0.0);
-		bus[21]= new busSpecification(22,0,1.0,0.0,0.0,0.0,0.0,0.0,0,0,0.0);
-		bus[22]= new busSpecification(23,0,1.0,0.0,3.2,1.6,0.0,0.0,0,0,0.0);
-		bus[23]= new busSpecification(24,0,1.0,0.0,8.7,6.7,0.0,0.0,0,0,0.0);
-		bus[24]= new busSpecification(25,0,1.0,0.0,0.0,0.0,0.0,0.0,0,0,0.0);
-		bus[25]= new busSpecification(26,0,1.0,0.0,3.5,2.3,0.0,0.0,0,0,0.0);
-		bus[26]= new busSpecification(27,0,1.0,0.0,0.0,0.0,0.0,0.0,0,0,0.0);
-		bus[27]= new busSpecification(28,0,1.0,0.0,0.0,0.0,0.0,0.0,0,0,0.0);
-		bus[28]= new busSpecification(29,0,1.0,0.0,2.4,0.9,0.0,0.0,0,0,0.0);
-		bus[29]= new busSpecification(30,0,1.0,0.0,10.6,1.9,0.0,0.0,0,0,0.0);
+		bus[0]= new busSpecification(0,0,1.05,0,0,0,165.9,0,0,0,0);
+		bus[1]= new busSpecification(1,1,1.043,0,21.7,12.7,49.1,28.4,-40,50,0);
+		bus[2]= new busSpecification(2,-1,1,0,2.4,1.2,0,0,0,0,0);
+		bus[3]= new busSpecification(3,-1,1,0,7.6,1.6,0,0,0,0,0);
+		bus[4]= new busSpecification(4,1,1.01,0,94.2,19,21.6,28,-40,40,0);
+		bus[5]= new busSpecification(5,-1,1,0,0,0,0,0,0,0,0);
+		bus[6]= new busSpecification(6,-1,1,0,22.8,10.9,0,0,0,0,0);
+		bus[7]= new busSpecification(7,1,1.01,0,30,30,22.8,39.1,-6,24,0);
+		bus[8]= new busSpecification(8,-1,1,0,0,0,0,0,0,0,0);
+		bus[9]= new busSpecification(9,-1,1,0,5.8,2,0,0,-6,24,0);
+		bus[10]= new busSpecification(10,1,1.082,0,0,0,12.4,31.6,0,0,0);
+		bus[11]= new busSpecification(11,-1,1,0,11.2,7.5,0,0,0,0,0);
+		bus[12]= new busSpecification(12,1,1.071,0,0,0,11.6,45.7,0,0,0);
+		bus[13]= new busSpecification(13,-1,1,0,6.2,1.6,0,0,0,0,0);
+		bus[14]= new busSpecification(14,-1,1,0,8.2,2.5,0,0,0,0,0);
+		bus[15]= new busSpecification(15,-1,1,0,3.5,1.8,0,0,0,0,0);
+		bus[16]= new busSpecification(16,-1,1,0,9,5.8,0,0,0,0,0);
+		bus[17]= new busSpecification(17,-1,1,0,3.2,0.9,0,0,0,0,0);
+		bus[18]= new busSpecification(18,-1,1,0,9.5,3.4,0,0,0,0,0);
+		bus[19]= new busSpecification(19,-1,1,0,2.2,0.7,0,0,0,0,0);
+		bus[20]= new busSpecification(20,-1,1,0,17.5,11.2,0,0,0,0,0);
+		bus[21]= new busSpecification(21,-1,1,0,0,0,0,0,0,0,0);
+		bus[22]= new busSpecification(22,-1,1,0,3.2,1.6,0,0,0,0,0);
+		bus[23]= new busSpecification(23,-1,1,0,8.7,6.7,0,0,0,0,0);
+		bus[24]= new busSpecification(24,-1,1,0,0,0,0,0,0,0,0);
+		bus[25]= new busSpecification(25,-1,1,0,3.5,2.3,0,0,0,0,0);
+		bus[26]= new busSpecification(26,-1,1,0,0,0,0,0,0,0,0);
+		bus[27]= new busSpecification(27,-1,1,0,0,0,0,0,0,0,0);
+		bus[28]= new busSpecification(28,-1,1,0,2.4,0.9,0,0,0,0,0);
+		bus[29]= new busSpecification(29,-1,1,0,10.6,1.9,0,0,0,0,0);
+
 
 		
 		linedata= new line[41];
 		
 		
-		linedata[0]= new line(1,2,0.0192,0.0575,0.0264,1,130.00);
-		linedata[1]= new line(1,3,0.0452,0.1652,0.0204,1,130.0000);
-		linedata[2]= new line(2,4,0.0570,0.1737,0.0184,1,65.0000);
-		linedata[3]= new line(3,4,0.0132,0.0379,0.0042,1,130);
-		linedata[4]= new line(2,5,0.0472,0.1983,0.0209,1,130);
-		linedata[5]= new line(2,6,0.0581,0.1763,0.0187,1,65);
-		linedata[6]= new line(4,6,0.0119,0.0414,0.0045,1,90);
-		linedata[7]= new line(7,5,0.0460,0.1160,0.0102,1,70);
-		linedata[8]= new line(6,7,0.0267,0.0820,0.0085,1,130);
-		linedata[9]= new line(6,8,0.0120,0.0420,0.0045,1,32);
-		linedata[10]= new line(6,9,0.0000,0.2080,0.0,1,65);
-		linedata[11]= new line(6,10,0.0000,0.5560,0.0,1,32);
-		linedata[12]= new line(11,9,0.0000,0.2080,0.0,1,65);
-		linedata[13]= new line(9,10,0.0000,0.1100,0.0,1,65);
-		linedata[14]= new line(4,12,0.0000,0.2560,0.0,1,65);
-		linedata[15]= new line(13,12,0.0000,0.1400,0.0,1,65);
-		linedata[16]= new line(12,14,0.1231,0.2559,0.0,1,32);
-		linedata[17]= new line(12,15,0.0662,0.1304,0.0,1,32);
-		linedata[18]= new line(12,16,0.0945,0.1987,0.0,1,32);
-		linedata[19]= new line(14,15,0.2210,0.1997,0.0,1,16);
-		linedata[20]= new line(16,17,0.0524,0.1923,0.0,1,16);
-		linedata[21]= new line(15,18,0.1073,0.2185,0.0,1,16);
-		linedata[22]= new line(18,19,0.0639,0.1292,0.0,1,16);
-		linedata[23]= new line(20,19,0.0340,0.0680,0.0,1,32);
-		linedata[24]= new line(10,20,0.0936,0.2090,0.0,1,32);
-		linedata[25]= new line(10,17,0.0324,0.0845,0.0,1,32);
-		linedata[26]= new line(10,21,0.0348,0.0749,0.0,1,32);
-		linedata[27]= new line(10,22,0.0727,0.1499,0.0,1,32);
-		linedata[28]= new line(22,21,0.0116,0.0236,0.0,1,32);
-		linedata[29]= new line(15,23,0.1000,0.2020,0.0,1,16);
-		linedata[30]= new line(22,24,0.1150,0.1790,0.0,1,16);
-		linedata[31]= new line(23,24,0.1320,0.2700,0.0,1,16);
-		linedata[32]= new line(25,24,0.1885,0.3292,0.0,1,16);
-		linedata[33]= new line(25,26,0.2544,0.3800,0.0,1,16);
-		linedata[34]= new line(27,25,0.1093,0.2087,0.0,1,16);
-		linedata[35]= new line(28,27,0.0000,0.3960,0.0,1,65);
-		linedata[36]= new line(27,29,0.2198,0.4153,0.0,1,16);
-		linedata[37]= new line(27,30,0.3202,0.6027,0.0,1,16);
-		linedata[38]= new line(29,30,0.2399,0.4533,0.0,1,16);
-		linedata[39]= new line(8,28,0.0636,0.2000,0.0214,1,32);
-		linedata[40]= new line(6,28,0.0169,0.0599,0.0065,1,32);
-		
+		linedata[0]= new line(0,1,0.0192,0.0575,0.0264,1,130);
+		linedata[1]= new line(0,2,0.0452,0.1652,0.0204,1,130);
+		linedata[2]= new line(1,3,0.057,0.1737,0.0184,1,65);
+		linedata[3]= new line(2,3,0.0132,0.0379,0.0042,1,130);
+		linedata[4]= new line(1,4,0.0472,0.1983,0.0209,1,130);
+		linedata[5]= new line(1,5,0.0581,0.1763,0.0187,1,65);
+		linedata[6]= new line(3,5,0.0119,0.0414,0.0045,1,90);
+		linedata[7]= new line(6,4,0.046,0.116,0.0102,1,70);
+		linedata[8]= new line(5,6,0.0267,0.082,0.0085,1,130);
+		linedata[9]= new line(5,7,0.012,0.042,0.0045,1,32);
+		linedata[10]= new line(5,8,0,0.208,0,1,65);
+		linedata[11]= new line(5,9,0,0.556,0,1,32);
+		linedata[12]= new line(10,8,0,0.208,0,1,65);
+		linedata[13]= new line(8,9,0,0.11,0,1,65);
+		linedata[14]= new line(3,11,0,0.256,0,1,65);
+		linedata[15]= new line(12,11,0,0.14,0,1,65);
+		linedata[16]= new line(11,13,0.1231,0.2559,0,1,32);
+		linedata[17]= new line(11,14,0.0662,0.1304,0,1,32);
+		linedata[18]= new line(11,15,0.0945,0.1987,0,1,32);
+		linedata[19]= new line(13,14,0.221,0.1997,0,1,16);
+		linedata[20]= new line(15,16,0.0524,0.1923,0,1,16);
+		linedata[21]= new line(14,17,0.1073,0.2185,0,1,16);
+		linedata[22]= new line(17,18,0.0639,0.1292,0,1,16);
+		linedata[23]= new line(19,18,0.034,0.068,0,1,32);
+		linedata[24]= new line(9,19,0.0936,0.209,0,1,32);
+		linedata[25]= new line(9,16,0.0324,0.0845,0,1,32);
+		linedata[26]= new line(9,20,0.0348,0.0749,0,1,32);
+		linedata[27]= new line(9,21,0.0727,0.1499,0,1,32);
+		linedata[28]= new line(21,20,0.0116,0.0236,0,1,32);
+		linedata[29]= new line(14,22,0.1,0.202,0,1,16);
+		linedata[30]= new line(21,23,0.115,0.179,0,1,16);
+		linedata[31]= new line(22,23,0.132,0.27,0,1,16);
+		linedata[32]= new line(24,23,0.1885,0.3292,0,1,16);
+		linedata[33]= new line(24,25,0.2544,0.38,0,1,16);
+		linedata[34]= new line(26,24,0.1093,0.2087,0,1,16);
+		linedata[35]= new line(27,26,0,0.396,0,1,65);
+		linedata[36]= new line(26,28,0.2198,0.4153,0,1,16);
+		linedata[37]= new line(26,29,0.3202,0.6027,0,1,16);
+		linedata[38]= new line(28,29,0.2399,0.4533,0,1,16);
+		linedata[39]= new line(7,27,0.0636,0.2,0.0214,1,32);
+		linedata[40]= new line(5,27,0.0169,0.0599,0.0065,1,32);
 
 		
-		this.setBounds(generateBounds(getDimension()));
 		
 		Vector<Double> pgTemp= new Vector<Double>();
 		Vector<Double> pdTemp= new Vector<Double>();
@@ -139,7 +137,7 @@ public class P9 extends Problem {
 			
 			if(bus[i].loadMW>0) {
 				pdTemp.add(bus[i].loadMW/100);
-				gTemp.add(i);
+				dTemp.add(i);
 			}
 			
 		}
@@ -152,22 +150,12 @@ public class P9 extends Problem {
 		bt=MatLab.zeros(pg.length, pd.length);
 		
 		//50 MW BILATERAL TRANSACTION 
-		bt[1][4]=0.05;   bt[1][5]=0.1;   bt[1][6]=0.05;
-		bt[2][3]=0.05;
-		bt[3][21]=0.025;
-		bt[4][21]=0.025; bt[4][16]=0.15;
-		bt[5][12]=0.025;
-		bt[6][8]=0.025;
-		
-		pg2=new double[pg.length];
-		pd2=new double[pd.length];
-		
-		for(int i=0; i<pg.length; i++) {
-			for(int j=0; j<pd.length; j++) {
-				pg2[i]+=bt[i][j];
-				pd2[j]+=bt[i][j];
-			}
-		}
+		bt[0][3]=0.05;   bt[0][4]=0.1;   bt[0][5]=0.05;
+		bt[1][2]=0.05;
+		bt[2][20]=0.025;
+		bt[3][20]=0.025; bt[3][15]=0.15;
+		bt[4][11]=0.025;
+		bt[5][7]=0.025;
 		
 		//Formation YIbus
 		YIbus=MatLab.zeros(bus.length-1);
@@ -204,6 +192,8 @@ public class P9 extends Problem {
 		}
 		
 		//PTDF
+		
+		ptdf= new double[gbus.length][dbus.length][linedata.length];
 		
 		for(int i=0; i<gbus.length; i++) {
 			for(int j=0; j<dbus.length; j++) {
@@ -315,8 +305,8 @@ public class P9 extends Problem {
 		
 		for(int i=0; i<pg.length; i++) {
 			for(int j=0; j<pd.length; j++) {
-				bounds[(i-1)*pd.length+j][0]=0;
-				bounds[(i-1)*pd.length+j][0]=Math.min(pg[i]-bt[i][j], pd[j]-bt[i][j]);
+				bounds[i*pd.length+j][0]=0;
+				bounds[i*pd.length+j][1]=Math.min(pg[i]-bt[i][j], pd[j]-bt[i][j]);
 			}
 		}
 				
